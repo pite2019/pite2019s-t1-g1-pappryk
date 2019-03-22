d2a48957e3bf68b1fa75eb915a4fff2e1065062b
pappryk <patryknng@interia.pl>
2019-03-22 12:50:09 +0100

    Split to 2 files

--- lab3/task.py
+++ lab3/task.py
@@ -1,35 +1,4 @@
-
-
-class Matrix:
-    def __init__(self, *values):
-        self.values = values
-
-    def add(self, matrix_to_add):
-        if not self.check_dimensions(matrix_to_add):
-            print("Different dimensions")
-            return None
-
-        new_matrix = self.create_new_matrix(matrix_to_add)
-        return new_matrix
-
-
-    def check_dimensions(self, matrix_to_add):
-        if len(self.values) == len(matrix_to_add.values):
-            return True
-        return False
-
-    def create_new_matrix(self, matrix_to_add):
-        new_matrix = []
-        for i in range(len(matrix_to_add.values)):
-            val = matrix_to_add.values[i] + self.values[i]
-            new_matrix.append(val)
-        return Matrix(new_matrix)
-    
-    def print_matrix(self):
-        for i in self.values:
-            print(i, end=' ')
-
-
+from matrix import Matrix
 
 
 def main():
